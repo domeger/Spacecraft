3512171e188a058768d9f1622b223fd5edd3674e
Stefanos Amanuel <stefano.amanuel@me.com>
2019-04-04 19:03:01 -0400

    changed docker start up script

---
+++ Dockerun.sh
@@ -0,0 +1,7 @@
+docker run -d -v /Spacecraft/data:/data -e VERSION=1.12.2 \
+
+	-e TYPE=FOREGE -e FORGEVERSION=14.23.5.2768 \
+
+	-p 25565:25565 -e EULA=TRUE --name spacecraft itzg/minecraft-server 
+
+
