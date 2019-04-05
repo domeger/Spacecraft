c3e7a7edc38254d559f9915297fad2b18d0eba52
ipkstef <stefano.amanuel@me.com>
2019-04-05 02:06:09 -0400

    fixed MAX_MEMORY

--- Dockerun.sh
+++ Dockerun.sh
@@ -1,5 +1,5 @@
-docker run -d -v /Spacecraft/data:/data -e VERSION=1.12.2 \
-	-e TYPE=FORGE  -e FORGEVERSION=14.23.5.2768 \
+docker run -d -v $PWD/data:/data -e VERSION=1.12.2 \
+	-e TYPE=FORGE  -e FORGEVERSION=14.23.5.2768 -e MAX_MEMORY=3g \
 	-p 25565:25565 -e EULA=TRUE --name spacecraft itzg/minecraft-server 
 
 
