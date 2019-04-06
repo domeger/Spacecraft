104895bbf1039050731192681772d3c192d21b7f
ipkstef <stefano.amanuel@me.com>
2019-04-06 00:28:39 -0400

    updated everything this is a pixelmon server

--- Dockerun.sh
+++ Dockerun.sh
@@ -2,9 +2,8 @@
 
 docker run -d -v $PWD/data:/data -e VERSION=1.12.2 \
 	-e TYPE=FORGE  -e FORGEVERSION=14.23.5.2768 -e MAX_MEMORY=3g \
-	-p 25565:25565 -e EULA=TRUE --name spacecraft itzg/minecraft-server 
+	-p 25565:25565 -e EULA=TRUE --name pokecraft itzg/minecraft-server 
 
 
-
-sleep 5
 $PWD/Galaticjar.sh
+
--- Galaticjar.sh
+++ Galaticjar.sh
@@ -1,5 +1,15 @@
+docker start pokecraft
+wait process_id=$!
 sleep 30
-mv $PWD/*.jar $PWD/data/mods
-
+docker stop pokecraft
+wait process_id=$!
 cp $PWD/server.properties.backup $PWD/data/server.properties
-
+if [-d "$PWD/data/mods"]; then 
+cd $PWD/data/mod && wget -O Pixelmon-1.12.2-7.0.5.jar https://dl.reforged.gg/2HKQPRx; fi
+wait process_id=$!
+docker start pokecraft
+wait process_id=$!
+sleep 30
+docker stop pokecraft
+pid=$!
+echo $pid Please start the server with Docker run pokecraft
