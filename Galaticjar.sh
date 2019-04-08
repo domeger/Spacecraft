cc2c86d31dcd9c6ed646bb16a980d393a10020c5
Stefanos Amanuel <saamanue@amazon.com>
2019-04-08 12:03:46 -0400

    testing ouf if then statements

--- Galaticjar.sh
+++ Galaticjar.sh
@@ -1,10 +1,18 @@
 docker start pokecraft
 wait 
-sleep 30
+sleep 5
 docker stop pokecraft
 wait 
 cp $PWD/server.properties.backup $PWD/data/server.properties
-cd $PWD/data/mods && wget -O Pixelmon-1.12.2-7.0.5.jar https://dl.reforged.gg/2HKQPRx;
+
+if [[-d "$PWD/data/mods"]];
+ 
+then 
+	cd $PWD/data/mods && wget -O Pixelmon-1.12.2-7.0.5.jar https://dl.reforged.gg/2HKQPRx
+else
+	echo 'mods folder doesn't exist, the Docker container might not have populated the data folder. Start stop Docker manually'
+fi 
+	
 wait 
 docker start pokecraft
 wait 
